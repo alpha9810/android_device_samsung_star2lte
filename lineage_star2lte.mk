683b0a726e13be2d9fcf0d7b7f2b3e88992a4731
saifanidk <saifan7787790@gmail.com>
2026-09-08 19:13:39 +0600

    Add build properties and flags for lineage_star2lte

--- lineage_star2lte.mk
+++ lineage_star2lte.mk
@@ -22,3 +22,18 @@ PRODUCT_MODEL := SM-G965F
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="samsung star2lte star2lte:16 BP4A.251205.006 eng.androi:user release-keys" \
+    BuildFingerprint=samsung/star2lte/star2lte:16/BP4A.251205.006/eng.androi:user/release-keys \
+    DeviceName=$(PRODUCT_SYSTEM_DEVICE) \
+    DeviceProduct=$(PRODUCT_SYSTEM_NAME) \
+    SystemDevice=$(PRODUCT_SYSTEM_DEVICE) \
+    SystemName=$(PRODUCT_SYSTEM_NAME)
+
+# some evox flags
+TARGET_ENABLE_FP_OVERRIDE ?= true
+WITH_GMS := false
+EVO_BUILD_TYPE := Unofficial
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_BUILD_APERTURE_CAMERA := true
