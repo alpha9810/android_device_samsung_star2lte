3e479bb5fa1d3425aa1b2bbaed526237d1a48911
saifanidk <saifan7787790@gmail.com>
2026-09-24 18:17:49 +0600

    Update product makefile for infinity_star2lte

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,9 +2,4 @@
 # Copyright (C) 2022 The LineageOS Project
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_star2lte.mk
-
-COMMON_LUNCH_CHOICES := \
-    lineage_star2lte-eng \
-    lineage_star2lte-user \
-    lineage_star2lte-userdebug
+    $(LOCAL_DIR)/infinity_star2lte.mk
